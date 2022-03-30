1282cac4a04a70c4c8dabe64f2fcefae205f6f06
Gabriel Mathias <gablemathias@icloud.com>
2022-03-30 17:40:45 -0300

    create and append weather data input

--- myWeatherApi/script.js
+++ myWeatherApi/script.js
@@ -1,14 +1,24 @@
 const cityValue = document.querySelector('.cityValue');
 const submitButton = document.querySelector('.button');
 
-const name = document.querySelector('.name');
-const description = document.querySelector('.description');
+const cityName = document.querySelector('.name');
 const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
 
 submitButton.addEventListener('click', () => {
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityValue.value+'&appid=38fb4a8d1ffb92242a9d58d93ec7af87')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+
+    const nameValue = document.createTextNode(data['name']);
+    const tempValue = document.createTextNode(data['main']['temp']);
+    const descValue = document.createTextNode(data['weather'][0]['description']);
+
+    cityName.appendChild(nameValue);
+    temperature.appendChild(tempValue);
+    description.appendChild(descValue);
+
+  })
 });
 
 
