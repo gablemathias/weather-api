5536adac12b8ee646ad5a5f9bb7105e434b02775
Gabriel Mathias <gablemathias@icloud.com>
2022-03-31 06:09:50 -0300

    remove unnecessary comments

--- myWeatherApi/script.js
+++ myWeatherApi/script.js
@@ -27,32 +27,6 @@ submitButton.addEventListener('click', async () => {
 
 });
 
-
-
-/*fetch('http://api.openweathermap.org/geo/1.0/direct?q='+cityValue.value+'&limit=5&appid=38fb4a8d1ffb92242a9d58d93ec7af87')
-.then(response => response.json())
-.then(data => console.log(data))
-*/
-/*fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.1289771&lon=-84.0832646&appid=38fb4a8d1ffb92242a9d58d93ec7af87');*/
-
-/*
-submitButton.addEventListener('click', () => {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityValue.value+'&appid=38fb4a8d1ffb92242a9d58d93ec7af87')
-  .then(response => response.json())
-  .then(data => {
-
-    const nameValue = document.createTextNode(data['name']);
-    const tempValue = document.createTextNode(Math.round(data['main']['temp']-273.15));
-    const descValue = document.createTextNode(data['weather'][0]['description']);
-
-    cityName.appendChild(nameValue);
-    temperature.appendChild(tempValue);
-    description.appendChild(descValue);
-
-  })
-});
-*/
-
 //Validar Input da Cidade
 //Tratar Erros (Try Catch) --> Nulo, Cidade Válida
 //Limpar Submit
